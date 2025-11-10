4efde6d3fa2b132a4048135939fe13fe06c4d895
JuansseVelez <juansserv2630@gmail.com>
2025-11-09 21:38:55 -0500

    Se agrego el archivo saludo.js solicitado

---
+++ ejercicios/Saludo.js
@@ -0,0 +1,15 @@
+// Ejercicio: Saludo 
+// Definición de variables
+
+// Variable con tu nombre
+const nombre = "Juan Sebastian Ramirez"; 
+
+// Variable con un saludo
+const saludo = "Hola";
+
+// Variable con el mensaje completo
+const mensajeCompleto = `${saludo} ${nombre}`;
+
+// Mostrar en consola
+console.log(mensajeCompleto);
+
